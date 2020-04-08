69e93ea270f3d6f36ebb6d41d14616efe925c56e
Emily Aquin <eaquin1@gmail.com>
2020-04-08 13:10:09 -0500

    refactored messages to user based on word entered

--- static/boggle.js
+++ static/boggle.js
@@ -1,15 +1,32 @@
 
-
+let wordList = new Set()
 
 $(".add-word").on("click", async function(e) {
     e.preventDefault();
-    console.log("here")
+    
     const $word = $("#guess");
 
-    let word = $word.val();
+    let word = $word.val().toLowerCase();
     if (!word) return;
 
+    if (wordList.has(word)) {
+        $(".msg").html("You've already guessed this word")
+        $word.val("").focus();
+        return
+    }
+
     response = await axios.get("/check-word", { params: { word:word}});
-    console.log(response)
+    let validity = response.data.result
+
+    if (validity === "ok") {
+        $(".msg").html("This word is correct!");
+        let newLi = $("<li>").text(word);
+        $(".words").append(newLi);
+        wordList.add(word);
+    } else if (validity === "not-word") {
+        $(".msg").html("This word is not in the English lanuage")
+    } else if (validity === "not-on-board") {
+        $(".msg").html("This word not on the board!")
+    }
     $word.val("").focus();
 });
